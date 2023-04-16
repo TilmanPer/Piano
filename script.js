c3a8710d47d3797998cb3f7d3de18a42fed07b29
Til Per <tilmanper@icloud.com>
2023-04-16 03:49:58 +0200

    Added save recordings feature and fixed some bugs on the recordNavigate

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let lastKey = 108; // C8
 let keyVelocity = 127; // 0-127
 let pianoLength = lastKey - firstKey + 1;
 let volume = 0;
-let pianoWidthPx = 0; 
+let pianoWidthPx = 0;
 
 const sustainKey = "Space";
 const sustainCode = 64; // MIDI Controller Code for Sustain Pedal
@@ -18,10 +18,8 @@ let playlist = [];
 let isRecording = false;
 let isPlaying = false;
 let timestamp = 0; // Used to calculate the timing of recorded events
-let songIndex = 0; 
-
 window.addEventListener('load', function (e) {
-    volume = volumeInput.value; 
+    volume = volumeInput.value;
     instructions.classList.add("fadeOut");
 });
 
@@ -97,26 +95,43 @@ function startRecording() {
 function stopRecording() {
     console.log("Recording Stopped");
     playlist.push(recordedEvents);
+    updateLocalPLaylist();
     /*const deleteButton = document.createElement("button");
     deleteButton.innerHTML = "⛔";
     deleteButton.addEventListener("click", function () {
         playlist.splice(playlist.length, 1);
         songSelect.remove(playlist.length);
-        songIndex--;
+        songSelect.selectedIndex
+    --;
         songSelect.selectedIndex--;
     });
 */ //TODO: Add delete button functionality
     songSelect.innerHTML += "<option>Song " + playlist.length + "</option>";
+    songSelect.selectedIndex = playlist.length-1;
     isRecording = false;
+}
+
+function updateLocalPLaylist() {
+    localStorage.setItem("playlist", JSON.stringify(playlist));
+}
+
+function loadLocalPlaylist() {
+    playlist = JSON.parse(localStorage.getItem("playlist"));
+    let index = 1;
+    playlist.forEach(song => {
+        songSelect.innerHTML += "<option>Song " + index + "</option>";
+        index++;
+    });
 }
 
 function playSong(index) {
     console.log("Playing Song " + index);
+    console.log(playlist[index]);
     if (!playlist[index]) {
         console.log("No song at index " + index);
         return;
     }
-    if (!isRecording && recordedEvents.length > 0) {
+    if (!isRecording && playlist.length > 0) {
         isPlaying = true;
         playlist[index].forEach(recordedEvent => {
             setTimeout(() => {
@@ -134,7 +149,8 @@ function playSong(index) {
 
 songSelect = document.getElementById("songSelect");
 songSelect.addEventListener("change", function () {
-    songIndex = songSelect.selectedIndex;
+    songSelect.selectedIndex
+ = songSelect.selectedIndex;
 });
 checkbox = document.getElementById('toggleRecording')
 checkbox.addEventListener('keydown', event => {
@@ -657,4 +673,6 @@ window.addEventListener('load', () => {
     loadPointsFromLocalStorage();
     updateCanvas();
     toggleCurveEditorDisplay();
+    if (localStorage.getItem('playlist'))
+        loadLocalPlaylist();
 });
